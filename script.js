154e2184b4761ccadf19086bd92c71114b9bff69
SjoerdGn <38759766+SjoerdGn@users.noreply.github.com>
2022-09-05 22:41:34 +0200

    helloworld example domestika course

---
+++ script.js
@@ -0,0 +1,42 @@
+
+let canvas = document.querySelector('canvas');
+// let canvas = document.getElementById('canvas');
+let context = canvas.getContext('2d');
+context.fillStyle = 'rgb(100,10,50)';
+context.fillRect(100, 100, 400, 400);
+
+context.lineWidth = 4;
+context.beginPath();
+context.rect(100,100,400,400);
+context.stroke();
+
+context.beginPath();
+context.arc(300,300, 100, 0,Math.PI*2);
+context.stroke();
+
+
+// let year = 2042;
+// let nextYear = year + 1;
+
+// let dinner = 'pasta';
+// let sentence = 'tonight, dinner will be ' + dinner;
+
+// function saySomething(param) {
+
+//     console.log(param);
+// }
+
+// saySomething(sentence);
+
+// function add(a, b) {
+//     return a + b;
+// }
+
+// let result = add(5, 7);
+// console.log(result);
+
+// let multiply = (a, b) => {
+//     return a * b;
+// }
+
+// console.log(multiply(5, 7));
